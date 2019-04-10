d57124ee73484ca693b7fcc163df32809afa8ce7
tca88 <trishnaamin@gmail.com>
2019-04-10 15:21:02 -0400

    import applyMiddleware, thunk and logger into index.js, and also compose to open up redux dev tools.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { CharacterListView } from "./views";
 import "./styles/App.css";
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
@@ -12,8 +14,11 @@ import rootReducer from "./reducers";
 // rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 ReactDOM.render(
