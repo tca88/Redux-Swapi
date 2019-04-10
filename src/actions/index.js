632babb5e038df83b943c87dea9d59568a913031
tca88 <trishnaamin@gmail.com>
2019-04-10 15:50:54 -0400

    build out the action that will be sending off the axios request to the SWAPI api. Import actions into starWarsReducer.js and create reducers for each action.

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,29 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+import axios from "axios";
+export const FETCH_SWAPI_START = "FETCH_SWAPI_START";
+export const FETCH_SWAPI_SUCCESS = "FETCH_SWAPI_SUCCESS";
+export const FETCH_SWAPI_FAILURE = "FETCH_SWAPI_FAILURE";
+
+export const getSwapiPeople = () => dispatch => {
+  // when making async calls, you first dispatch a start action, then start the API call.
+  dispatch({ type: FETCH_SWAPI_START });
+  axios
+    .get("https://swapi.co/api/people")
+    .then(res => {
+      dispatch({
+        type: FETCH_SWAPI_SUCCESS,
+        payload: res.data
+      });
+      console.log(res);
+    })
+    .catch(err => {
+      dispatch({
+        type: FETCH_SWAPI_FAILURE,
+        payload: err
+      });
+      console.log(err);
+    });
+};
